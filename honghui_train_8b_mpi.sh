63676432cb1f438d161c0667bfb2241fa5128aa2
baohonghui <baohonghui@kuaishou.com>
2026-01-26 16:23:08 +0800

    fix: docs

--- honghui_train_8b_mpi.sh
+++ honghui_train_8b_mpi.sh
@@ -23,7 +23,7 @@ echo "OUTPUT: $output_dir"
 echo "--------------------------------------"
 
 # Multi-Node Launch using mpirun
-# Note: DeepSpeed can also be launched via mpirun by passing the rank/size info
+# We use a bash wrapper to map MPI rank variables to PyTorch/Transformers environment variables
 mpirun --allow-run-as-root \
     --hostfile $HOSTFILE \
     -x http_proxy -x https_proxy -x no_proxy \
@@ -35,25 +35,37 @@ mpirun --allow-run-as-root \
     -x PATH="$CONDA_ENV_PATH/bin:$PATH" \
     -x LD_LIBRARY_PATH="$CONDA_ENV_PATH/lib:$LD_LIBRARY_PATH" \
     -x PYTHONPATH="$CONDA_ENV_PATH/lib/python3.11/site-packages:$PYTHONPATH" \
-    $PYTHON_EXE -u src/train.py \
-    --deepspeed examples/deepspeed/ds_z2_config.json \
-    --stage sft \
-    --model_name_or_path $model_path \
-    --do_train \
-    --dataset meta2sid_dataset,rec_dataset \
-    --use_reasoner \
-    --reasoner_layers 1 \
-    --template qwen3 \
-    --finetuning_type full \
-    --output_dir $output_dir \
-    --overwrite_cache \
-    --save_total_limit 3 \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 8 \
-    --lr_scheduler_type cosine \
-    --logging_steps 10 \
-    --save_steps 200 \
-    --learning_rate 1e-4 \
-    --num_train_epochs 3.0 \
-    --plot_loss \
-    --bf16
+    bash -c '
+        if [ -n "$OMPI_COMM_WORLD_RANK" ]; then
+            export RANK=$OMPI_COMM_WORLD_RANK
+            export LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
+            export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
+        elif [ -n "$PMI_RANK" ]; then
+            export RANK=$PMI_RANK
+            export LOCAL_RANK=$PMI_LOCALRANKID
+            export WORLD_SIZE=$PMI_SIZE
+        fi
+        
+        $PYTHON_EXE -u src/train.py \
+            --deepspeed examples/deepspeed/ds_z2_config.json \
+            --stage sft \
+            --model_name_or_path '"$model_path"' \
+            --do_train \
+            --dataset meta2sid_dataset,rec_dataset \
+            --use_reasoner \
+            --reasoner_layers 1 \
+            --template qwen3 \
+            --finetuning_type full \
+            --output_dir '"$output_dir"' \
+            --overwrite_cache \
+            --save_total_limit 3 \
+            --per_device_train_batch_size 2 \
+            --gradient_accumulation_steps 8 \
+            --lr_scheduler_type cosine \
+            --logging_steps 10 \
+            --save_steps 200 \
+            --learning_rate 1e-4 \
+            --num_train_epochs 3.0 \
+            --plot_loss \
+            --bf16
+    '
